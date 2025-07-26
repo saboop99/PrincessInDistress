06600c97875a92a14737c4779f908651b6e54750
Saboop <rafa4021@hotmail.com>
2025-07-25 22:03:52 -0300

    Update - Movement, Jump and Sprint
    Fixed jump issues, now the player can jump higher and smootlhy, and won't interfere in the player movement
    
    Added a sprint mechanic

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,8 +5,10 @@ public class Player : MonoBehaviour
 {
     //Criando variáveis de velocidade, força do pulo, Rigidbody para ter fisica, direção do movimento e bool para checagem de pulo
     public float moveSpeed = 5f;
-    public float jumpForce = 100f;
+    public float sprintSpeed = 10f;
+    public float jumpForce = 20f;
     private bool isGrounded = false;
+    private bool isSprinting = false;
 
     private Rigidbody2D rb;
     private Vector2 movement;
@@ -21,22 +23,20 @@ public class Player : MonoBehaviour
     {
         // Coleta o input do teclado (clássico)
         movement.x = Input.GetAxisRaw("Horizontal");
-        
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
+
+        // Faz o personagem pular ao apertar Espaço
+        if (Input.GetButtonDown("Jump"))
         {
             if (isGrounded == true)
-            {
-                rb.AddForce(Vector2.up * jumpForce);
+            {      
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             }
-            //Console.WriteLine("espaço");
-            // Aplica força de pulo verticalmente
-            
-            //rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce));
-
         }
     }
 
+    // Checagem de chão sendo feita para controle de pulo do player
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -52,10 +52,10 @@ public class Player : MonoBehaviour
     }
 
     // Método FixedUpdate é chamado em intervalos fixos, ideal para física e movimentação com Rigidbody
+    // Definição da velocidade do jogador, calculando entre Correr e Andar
     void FixedUpdate()
     {
-        // Move o Rigidbody2D aplicando a direção e velocidade desejada
-        // Multiplica pelo deltaTime para manter o movimento consistente em diferentes framerates
-        rb.MovePosition(rb.position + new Vector2(movement.x, 0) * moveSpeed * Time.fixedDeltaTime);
-    }
+        float speed = isSprinting ? sprintSpeed : moveSpeed; 
+        rb.linearVelocity = new Vector2(movement.x * speed, rb.linearVelocity.y);
+    }         
 }
