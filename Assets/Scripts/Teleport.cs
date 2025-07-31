5a97818182001b6f962c85bafe8d7164bb028d6c
Saboop <rafa4021@hotmail.com>
2025-07-30 21:37:24 -0300

    finished the Teleport script

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -2,19 +2,38 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
+    /* 
+    Esse código faz o jogador se transportar automaticamente de um ponto para o outro ao entrar em uma colisão do tipo Trigger em determinadas portas e ao apertar a Tecla T,
+    com uma lógica usando OnTriggerEnter/Exit e Bool
+    */
+
     public Transform doorTeleport;
+    private Transform playerTeleport;
+    public bool playerInside;
+
+    private void Update()
+    {
+        if (playerInside && Input.GetKeyDown(KeyCode.T))
+        {
+            playerTeleport.position = doorTeleport.position;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player1"))
         {
-            other.transform.position = doorTeleport.position;
-            /*
-            if (Input.GetKeyDown(KeyCode.T))
-            {
-                other.transform.position = doorTeleport.position;
-            }*/
+            playerInside = true;
+            //other.transform.position = doorTeleport.position;
+            playerTeleport = other.transform;
         }
     }
-
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player1"))
+        {
+            playerInside = false;
+            playerTeleport = null;
+        }
+    }
 }
